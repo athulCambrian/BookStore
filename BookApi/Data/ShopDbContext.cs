a03ce0de4e420f857bb24b439ed50c3033080f23
zeezaglobal <96932409+zeezaglobal@users.noreply.github.com>
2022-12-03 17:31:10 -0800

    created test project created api project created blazor project created model classes added dbcontext for database migration Done the migration Added some data using migration database connection string added nuget packages for database connection added

---
+++ BookApi/Data/ShopDbContext.cs
@@ -0,0 +1,62 @@
+﻿using BookApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApi.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options):base (options){
+
+}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //Products
+            //Beauty Category
+            modelBuilder.Entity<Book>().HasData(new Book
+            {
+                Id = 1,
+                Name = "Harry Potter",
+                Author = "RR",
+               CategoryId=2,
+                Price = 100,
+               
+
+            });
+
+            modelBuilder.Entity<Book>().HasData(new Book
+            {
+                Id = 2,
+                Name = "KK",
+                Author = "KK",
+                CategoryId = 1,
+
+                Price = 100,
+               
+
+            });
+
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                Id = 1,
+                CategoryName = "Fantacy",
+               
+            });
+            modelBuilder.Entity<Category>().HasData(new Category
+            {
+                Id = 2,
+                CategoryName = "Horror",
+
+            });
+           
+
+
+        }
+
+      
+        public DbSet<BookCart> BookCarts { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Book> Books { get; set; }
+      
+    }
+}
---
+++ BookApi/Migrations/20221204012728_Initial.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Categorys");
+
+            migrationBuilder.RenameTable(
+                name: "CartItems",
+                newName: "BookCarts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categorys",
+                table: "Categorys",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BookCarts",
+                table: "BookCarts",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "CategoryId", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "RR", 2, "Harry Potter", 100 },
+                    { 2, "KK", 1, "KK", 100 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categorys",
+                table: "Categorys");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BookCarts",
+                table: "BookCarts");
+
+            migrationBuilder.RenameTable(
+                name: "Categorys",
+                newName: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "BookCarts",
+                newName: "CartItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CartItems",
+                table: "CartItems",
+                column: "Id");
+        }
+    }
+}
