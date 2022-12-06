c089c2b6a88d6b563d1dfbde528ad6179006c54b
meghnaaravind <A00256942@mycambrian.ca>
2022-12-06 16:32:59 -0500

    test commit

---
+++ BookStore/Pages/ProductBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BookStore.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+using BookStore.Models.Dtos; //(soln for error in ln 14)
+
+namespace BookStore.Pages
+{
+	public class ProductBase:ComponentBase
+	{
+
+		[Inject]
+	  public iProductService ProductService { get; set; }
+
+		public IEnumerable<ProductDto> Products { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Products = await ProductService.GetItems();
+             
+        }
+    }
+}
+
---
+++ BookStore/Services/Contracts/iProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+// using BookStore.Models.Dtos;
+
+namespace BookStore.Services.Contracts
+{
+	public interface iProductService
+	{
+		Task<IEnumerable<ProductDto>> GetItems();
+	}
+}
+
