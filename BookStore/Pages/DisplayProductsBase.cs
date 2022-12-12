87ee51999cdddb27d2dfcb799b35136dd8805a25
meghnaaravind <A00256942@mycambrian.ca>
2022-12-12 15:38:50 -0500

    created a child razor component name displayrazor.razor  for products.razor file to house the code that outputs the products’ relevant data.

---
+++ BookStore/Pages/DisplayProductsBase.cs
@@ -0,0 +1,13 @@
+﻿//using System;
+using Microsoft.AspNetCore.Components;
+using BookStore.Models.Dtos; 
+
+namespace BookStore.Pages
+{
+	public class DisplayProductsBase:ComponentBase 
+	{
+		[Parameter]
+		public IEnumerable<ProductDto> Products { get; set; }
+	} 
+}
+
