e41f6040704a8e70738fb497642e434ec180987b
meghnaaravind <A00256942@mycambrian.ca>
2022-12-13 17:15:57 -0500

    implemented code to display details regarding a specific product.

---
+++ BookStore.Web/Pages/ProductDetailsBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace BookStore.Web.Pages
+{
+	public class ProductDetailsBase
+	{
+		[Parameter]
+		public int id { get; set; }
+
+		public IProductService ProductService { get; set; }
+
+		public BooksDto BookDetails { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                ShoppingCartItems = await ManageCartItemsLocalStorageService.GetCollection();
+                Product = await GetProductById(Id);
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+    }
+}
+
