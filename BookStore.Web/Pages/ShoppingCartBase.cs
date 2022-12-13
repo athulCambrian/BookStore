1427b4501542810a66a40450e24584c8067a0a5d
meghnaaravind <A00256942@mycambrian.ca>
2022-12-13 16:49:57 -0500

    implemented code for displaying collections of products data for products added to the users’ shopping cart.

---
+++ BookStore.Web/Pages/ShoppingCartBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+//using ShopOnline.Models.Dtos;
+//using ShopOnline.Web.Services.Contracts;
+
+namespace BookStore.Web.Pages
+{
+    public class ShoppingCartBase : ComponentBase
+    {
+       
+
+        [Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+
+        public IEnumerable<BooksDto> ShoppingCartService { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserID);
+
+            }
+            catch (Exception ex)
+            {
+
+                ErrorMessage = ex.Message;
+            }
+        }
